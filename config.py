528bb4866a5be8e107714c49f9107de15014ce00
╰☆☆ᴍʀ ʀᴏᴍᴇᴏ☆☆╮ <126165195+itsRomeo143@users.noreply.github.com>
2024-01-04 00:27:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://rb.gy/4s12zc",
+    "https://github.com/itsRomeo143/Hindixmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
